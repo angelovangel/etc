29809ca4a0c4ca91b53601cb7a09e769f4d66da6
angelovangel <aangeloo@gmail.com>
2023-09-28 15:16:46 +0300

    add get-phred-frompaf script

---
+++ bin/get-phred-frompaf.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+# get the qscore from a paf with a de:f tag (as in minimap2 -c)
+
+awk '$12 >= 60' | grep -o 'de:f:[.0-9]*' | cut -d: -f3 | awk '{print -10*(log($1)/log(10))}' < "${1:-/dev/stdin}"
