65be0728f2625c5198bbfbe7746ca0e11d366366
angelovangel <aangeloo@gmail.com>
2024-01-31 11:53:44 +0300

    add option to mimic MinKNOW output folder structure

--- bin/ont-basecall.sh
+++ bin/ont-basecall.sh
@@ -12,16 +12,18 @@ Options:
     -p  (required) path to ONT pod5 folder
     -k  (optional) barcoding kit, if used demultiplexing will be performed (SQK-NBD114-96, SQK-RBK114-96 ...)
     -r  (optional flag) find pod5 files recursively
-    -b  (optional flag) save reads in bam files, (fastq.gz by default)"
+    -b  (optional flag) save reads in bam files, (fastq.gz by default)
+    -f  (optional flag) save reads in barcodeXX folders (mimic MinKNOW output)"
 
 recurs=false
 bam=false
+folders=false
 
 unset -v model
 unset -v podpath
 unset -v kit
 
-while getopts :hrbm:p:k: flag
+while getopts :hrbfm:p:k: flag
 do
    case "${flag}" in
       h) echo "$usage"; exit;;
@@ -30,6 +32,7 @@ do
       k) kit=${OPTARG};;
       r) recurs=true;;
       b) bam=true;;
+      f) folders=true;;
       :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
      \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
    esac
@@ -49,7 +52,7 @@ else
 fi
 # check for output directory, make it same level as the main run folder, e.g. parent of pod5 dir
 run_directory=$(dirname $podpath)
-output_directory=$(dirname $podpath)/basecalled-$model
+output_directory=$(dirname $podpath)/basecall-$model
 
 [ -d $output_directory ] && \
 echo -e "Basecalled folder exists, will be deleted ...\n==============================" && \
@@ -88,12 +91,12 @@ SECONDS=0
 echo "------------------------"
 if [[ $demux == 'false' ]]; then
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] - starting basecalling (${model} model), using dorado version ${dorado_version}" | \
-    tee -a $output_directory/basecall.log
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] - found ${npod5files} pod5 files" | tee -a $output_directory/basecall.log
+    tee -a $output_directory/0_basecall.log
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] - found ${npod5files} pod5 files" | tee -a $output_directory/0_basecall.log
 else
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] - starting basecalling (${model} model) and demultiplexing (${kit}), using dorado version ${dorado_version}" | \
-    tee -a $output_directory/basecall.log
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] - found ${npod5files} pod5 files" | tee -a $output_directory/basecall.log
+    tee -a $output_directory/0_basecall.log
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] - found ${npod5files} pod5 files" | tee -a $output_directory/0_basecall.log
 fi
 echo "------------------------"
 
@@ -107,8 +110,21 @@ fi
 echo "------------------------"
 echo "Elapsed time: $SECONDS seconds"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] - finished basecalling of ${npod5files} files, data is in $(realpath $output_directory)" | \
-tee -a $output_directory/basecall.log
+tee -a $output_directory/0_basecall.log
 echo "------------------------"
+
+# if we want to mimic the output of MinKNOW realtime basecalling, we have to make a directory for each barcode and put the file there
+if [ $folders == 'true' -a $demux == 'true' ]; then
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] - moving files to barcode folders" | tee -a $output_directory/0_basecall.log    
+    for i in $output_directory/*.fastq; do
+        bc=$(basename $i .fastq | cut -d_ -f2); 
+        bcdir=$(dirname $i)/$bc; 
+        mkdir -p $bcdir && mv $i $bcdir/; 
+    done
+fi
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] - done!" | tee -a $output_directory/0_basecall.log
+
 # clean up
 # rm -rf .temp_dorado*
 
